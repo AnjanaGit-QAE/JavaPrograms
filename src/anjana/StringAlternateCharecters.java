e8bc16892673d77de0d7b247066d9efa7c51971d
Anjana <rajaanjanasharanya1205@gmail.com>
2024-10-03 21:00:01 +0530

    New Code Added

--- src/anjana/StringAlternateCharecters.java
+++ src/anjana/StringAlternateCharecters.java
@@ -6,19 +6,39 @@ public class StringAlternateCharecters {
         
     	
     	String str = "AnjanaSharanyaRaja";
-        char[] charArr = str.toCharArray();
-        int len = charArr.length;
-        String newStr="";
-       
         
-        for(int i=0;i<len;i++) {
+        StringBuilder alternateChars = new StringBuilder();
+        StringBuilder remainingChars = new StringBuilder();
+
+        // Loop through the string to collect alternate characters
+        for (int i = 0; i < str.length(); i++) {
+            
+        	if (i % 2 == 0) {
+                alternateChars.append(str.charAt(i)); // Collect alternate characters (even index)
+            } 
         	
-        	if(i%2!=0) {
-        		newStr+=charArr[i];
-        	}
+        	else {
+                remainingChars.append(str.charAt(i)); // Collect remaining characters (odd index)
+            }
         }
-        System.out.println("New String is : " + newStr);
+
+        // Combine the alternate and remaining characters
+        System.out.println("Original String: " + str);
+        System.out.println("Resulting String: " + alternateChars.toString() + remainingChars.toString());
         
     }
 }
+
+/*
+ * char[] charArr = str.toCharArray(); 
+ * int len = charArr.length; String
+ * newStr=""; 
+ * for(int i=0;i<len;i++)
+ * {
+ * if(i%2!=0) {
+ *      newStr+=charArr[i]; 
+ *   } 
+ * }
+ *   System.out.println("New String is : " + newStr);
+ */
 
